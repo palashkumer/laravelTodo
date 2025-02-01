d20ab518678eb193b4bd0d1b26aa2210d8563d9a
Palash Kumer <palashkumer237@gmail.com>
2025-02-02 02:09:07 +0600

    create ProductController initial

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // This method will show products page
+    public function index() {}
+
+    // This method will create products 
+    public function create() {}
+
+    // This method will store products in db
+    public function store() {}
+
+    // This method will show products page
+    public function edit() {}
+
+    // This method will update products
+    public function update() {}
+
+    // This method will delete products 
+    public function destroy() {}
+}
